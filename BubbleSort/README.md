c9ce3ba8e37b9b26273bb6419466c73906a0e8b4
MateusGildev <mateuss.gill@hotmail.com>
2023-08-27 14:46:37 -0300

    ...

---
+++ BubbleSort/README.md
@@ -0,0 +1,5 @@
+# BubbleSortexe
+
+Este é um programa simples em Java que demonstra o algoritmo de ordenação Bubble Sort aprendido e revisado na aula de Estrutura de Dados. O Bubble Sort é um dos algoritmos mais simples de ordenação, mas não é muito eficiente para grandes conjuntos de dados. Ele funciona comparando e trocando elementos adjacentes até que todo o conjunto de dados esteja ordenado.
+
+Nesse programa, utilizei a biblioteca random para gerar 5 numeros de 0 a 100, mas fique a vontade para fazer suas proprias modificações.
---
+++ InsertionSort/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] vetor = new int[5];
+        int i, j, eleito, count;
+
+        System.out.println("vetor desordenado:");
+        for (count = 0; count < vetor.length; count++) {
+            vetor[count] = random.nextInt(100);
+            System.out.print(vetor[count]);
+
+            if (count < vetor.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        // Ordenação por inserção
+        for (i = 1; i < 5; i++) {
+            eleito = vetor[i];
+            j = i - 1;
+            while (j >= 0 && vetor[j] > eleito) {
+                vetor[j + 1] = vetor[j];
+                j--;
+            }
+            vetor[j + 1] = eleito;
+        }
+
+        System.out.println("vetor ordenado:");
+        for (i = 0; i < 5; i++) {
+            System.out.print(vetor[i]);
+
+            if (i < 4) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
+
+
